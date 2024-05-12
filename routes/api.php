f9bcc7474eb137010940beca70df25a90c7ee43b
pattyxica24 <pattyxica24@gmail.com>
2024-05-11 23:05:42 -0300

    authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\StoreController;
 use Illuminate\Http\Request;
@@ -16,9 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:sanctum')->group(function () {
 Route::resource('books', BookController::class);
 Route::resource('stores', StoreController::class);
+});
